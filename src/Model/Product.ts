3c9cd5369b83bc633a577499475da690c10bec19
Weslley David <weslleyvwp@hotmail.com>
2023-02-03 18:10:52 -0300

    feat: paginação para supabase

--- src/Model/Product.ts
+++ src/Model/Product.ts
@@ -1,6 +1,11 @@
 import { supabase } from "./supabase"
 
 export class ProductRepository {
+    getPagination(page: number, quantity: number) {
+        quantity = quantity
+        let start = (page * quantity), final = (page * quantity + quantity - 1)
+        return { start, final }
+    }
     findProduct = async (field: string, value: string) => {
         const { data, error } = await supabase
             .from('product')
@@ -12,4 +17,30 @@ export class ProductRepository {
         }
         return data
     }
+
+    listProducts = async (page: number) => {
+        const { start, final } = this.getPagination(page, 3);
+        const { data, error } = await supabase
+            .from('product')
+            .select()
+            //.order("id", { ascending: true })
+            .range(start, final)
+        if (error) {
+            throw new Error("not fetched")
+        }
+        return data
+    }
 }
+
+async function teste() {
+
+    let a = new ProductRepository()
+    // console.log(a.getPagination(0, 3))
+    // console.log(a.getPagination(1, 3))
+    // console.log(a.getPagination(2, 3))
+    console.log(await a.listProducts(2))
+}
+teste()
+
+
+
